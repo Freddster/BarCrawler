daa4d7330c10b8971222137c1abed40e8c2074ce
Nina Wiborg <nina_wiborg@hotmail.com>
2017-05-04 14:58:11 +0200

    MEGA GOD DATABASEKOMMUNIKATION PÅ BARPROFILEN!

--- BarCrawler/BarCrawler/Controllers/BarProfilController.cs
+++ BarCrawler/BarCrawler/Controllers/BarProfilController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BarCrawler.Models;
+using System.Data.Entity;
+using System.Net;
 
 namespace BarCrawler.Controllers
 {
+
+    
     public class BarProfilController : Controller
     {
+
+        private BarCrawlerContext db = new BarCrawlerContext();
+
         // GET: BarProfil
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BarModel barmodel = db.BarModels.Include(i => i.Pictures).Include(e => e.Events).Include(f => f.Feeds).Include(d => d.Drinks).SingleOrDefault(x => x.BarID == id);
+            if (barmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(barmodel); 
         }
     }
 }
