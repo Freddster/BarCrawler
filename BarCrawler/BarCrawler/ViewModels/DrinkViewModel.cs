07309beb094a4005c35c9dfa11b47a51fdd44720
Nina Wiborg <nina_wiborg@hotmail.com>
2017-05-12 18:21:28 +0200

    Rediger Drink

--- BarCrawler/BarCrawler/ViewModels/DrinkViewModel.cs
+++ BarCrawler/BarCrawler/ViewModels/DrinkViewModel.cs
@@ -14,27 +14,26 @@ namespace BarCrawler.ViewModels
         {
             
         }
-        public DrinkViewModel(DrinkModel dm, string str = "http://www.nice.com/PublishingImages/Career%20images/J---HR_Page-4st-strip-green-hair%20(2).png?RenditionID=-1")
+        public DrinkViewModel(DrinkModel dm)
         {
-            Drinkmodel = dm;
-            Picture = str;
-
-
+            DrinkID = dm.DrinkID; 
+            BarID = dm.BarID;
+            Title = dm.Title;
+            Description = dm.Description;
+            Price = dm.Price; 
         }
+        [Key]
+        public int DrinkID { get; set; }
 
-        public DrinkModel Drinkmodel { get; set; }
-        public string Picture { get; set; }
+        [ForeignKey("BarModel")]
+        public int BarID { get; set; }
 
-        //[ForeignKey("BarModel")]
-        //public int BarID { get; set; }
-        //public BarModel BarModel { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
 
-        //[Required]
-        //public string Title { get; set; }
-        //[MaxLength(500)]
-        //public string Description { get; set; }
-
-        //[Required]
-        //public string Price { get; set; }
+        [Required]
+        public string Price { get; set; }
     }
 }
