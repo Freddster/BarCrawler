230794c25b04bdd16658d4ee4c90ceb43cf9308b
au533036 <frederik.rosenoern.jensen@post.au.dk>
2017-05-15 16:39:25 +0200

    Løkkert feed der

--- BarCrawler/BarCrawler/Controllers/AccountController.cs
+++ BarCrawler/BarCrawler/Controllers/AccountController.cs
@@ -152,7 +152,7 @@ namespace BarCrawler.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.BarName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- BarCrawler/BarCrawler/Controllers/BarProfilController.cs
+++ BarCrawler/BarCrawler/Controllers/BarProfilController.cs
@@ -82,5 +82,34 @@ namespace BarCrawler.Controllers
                 return View(editviewmodel);
             }
         }
+
+
+        //Virker stadig ikke 
+        [HttpGet]
+        public ActionResult CreateFeed(int id , string t/**/)
+        {
+            if (!t.IsNullOrWhiteSpace())
+            {
+                FeedModel feed = new FeedModel();
+                feed.Text = t;
+                feed.BarID = id;
+                db.FeedModels.Add(feed);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", new {id = id});
+        }
+
+        [HttpGet]
+        public ActionResult DeleteFeed(int id, int Fid/**/)
+        {
+            var feed = this.db.FeedModels.FirstOrDefault(b => b.FeedID == Fid);
+            if (feed != null)
+            {
+                db.FeedModels.Remove(feed);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", new { id = id });
+        }
     }
 }
